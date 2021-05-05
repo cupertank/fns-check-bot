b58f12de8a5e42c82bdc7cc0a8d1670794c9ea3d
Ilya Vologin <ilyavologin@gmail.com>
2021-05-05 17:13:34 +0300

    Init bot branch

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update
 
 import fns_api
 
 TOKEN = os.getenv("TOKEN")
+
+
+def start_handler(update: Update, _: CallbackContext):
+    update.effective_message.reply_text("Привет!")
 
 
 def echo_handler(update: Update, _: CallbackContext):
@@ -14,6 +18,7 @@ def echo_handler(update: Update, _: CallbackContext):
 
 def main():
     updater = Updater(TOKEN)
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo_handler))
     updater.start_polling()
     updater.idle()
