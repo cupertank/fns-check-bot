82b1cb6ae60a44178c0fba573af78d753521ba00
Ilya Vologin <ilyavologin@gmail.com>
2021-05-05 12:18:25 +0300

    Added base echo realisation

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram import Update
+
+TOKEN = os.getenv("TOKEN")
+
+
+def echo_handler(update: Update, _: CallbackContext):
+    text = update.effective_message.text
+    update.effective_message.reply_text(text)
+
+
+def main():
+    updater = Updater(TOKEN)
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo_handler))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
+
