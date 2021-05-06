414ec9b3ad0542413e2bf97f245990e0edc14456
FerrumBrain <lemur1416@gmail.com>
2021-05-06 13:07:23 +0300

    Reading users' names

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ class Bot:
             entry_points=[CommandHandler('start', self.start_handler)],
             states={
                 1: [MessageHandler(Filters.text & ~Filters.command, self.phone_handler)],
-                2: [MessageHandler(Filters.text & ~Filters.command, self.code_handler)]
+                2: [MessageHandler(Filters.text & ~Filters.command, self.code_handler)],
+                3: [MessageHandler(Filters.text & ~Filters.command, self.guest_name_handler)]
             },
             fallbacks=[CommandHandler('cancel', self.cancel_handler)],
         )
@@ -59,7 +60,7 @@ class Bot:
         mess = update.effective_message.text
 
         if self.__is_correct_code(mess):
-            update.effective_message.reply_text('Верный код')
+            update.effective_message.reply_text('Верный код. Введите имена пользователей(для каждого пользователя введите имя на новой строчке):')
             return 3
 
         update.effective_message.reply_text("Неверный код, если хотите прекратить работу, введите /cancel")
@@ -68,6 +69,12 @@ class Bot:
     def cancel_handler(self, update: Update, _: CallbackContext):
         update.effective_message.reply_text('До новых встреч')
         return ConversationHandler.END
+
+    def guest_name_handler(self, update: Update, context: CallbackContext):
+        mess = update.effective_message.text
+        context.user_data["names"] = mess.splitlines()
+        update.effective_message.reply_text('Пришлите фотографию QR-кода с чека:')
+        return 4
 
     def run(self):
         self.updater.start_polling()
