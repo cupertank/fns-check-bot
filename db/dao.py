064f740851b9c0b27e0ef1d0b66b296e3e2972e1
Alex Kovrigin <a.kovrigin0@gmail.com>
2021-05-08 17:34:17 +0300

    database session id

--- db/dao.py
+++ db/dao.py
@@ -1,6 +1,6 @@
-from pypika import PostgreSQLQuery, Table
+from typing import Optional
 
-from .exceptions import FNSUserDoesNotExistException
+from pypika import PostgreSQLQuery, Table
 
 
 class Dao:
@@ -20,24 +20,52 @@ class Dao:
 
     def set_refresh_token(self, uid: int, refresh_token: str) -> None:
         if self.__isLocal:
-            self.__localDict[uid] = refresh_token
+            if "uid" not in self.__localDict:
+                self.__localDict[uid] = [None, None]
+            self.__localDict[uid][0] = refresh_token
             return
 
-        sql = PostgreSQLQuery.into(self.users).insert(uid, refresh_token)\
+        sql = PostgreSQLQuery.into(self.users).insert(uid, refresh_token, None) \
             .on_conflict(self.users.uid).do_update(self.users.refresh_token, refresh_token)
         self.cursor.execute(str(sql))
         self.db.commit()
 
-    def get_refresh_token(self, uid: int) -> str:
+    def get_refresh_token(self, uid: int) -> Optional[str]:
         if self.__isLocal:
             if uid in self.__localDict:
-                return self.__localDict[uid]
+                return self.__localDict[uid][0]
             else:
-                raise FNSUserDoesNotExistException(f'{uid} is not present in the database')
+                return None
 
         sql = PostgreSQLQuery.from_(self.users).select("refresh_token").where(self.users.uid == uid)
         self.cursor.execute(str(sql))
         res = self.cursor.fetchone()
         if res is None:
-            raise FNSUserDoesNotExistException(f'{uid} is not present in the database')
+            return None
+        return res[0]
+
+    def set_session_id(self, uid: int, session_id: str) -> None:
+        if self.__isLocal:
+            if "uid" not in self.__localDict:
+                self.__localDict[uid] = [None, None]
+            self.__localDict[uid][1] = session_id
+            return
+
+        sql = PostgreSQLQuery.into(self.users).insert(uid, None, session_id) \
+            .on_conflict(self.users.uid).do_update(self.users.session_id, session_id)
+        self.cursor.execute(str(sql))
+        self.db.commit()
+
+    def get_session_id(self, uid: int) -> Optional[str]:
+        if self.__isLocal:
+            if uid in self.__localDict:
+                return self.__localDict[uid][1]
+            else:
+                return None
+
+        sql = PostgreSQLQuery.from_(self.users).select("session_id").where(self.users.uid == uid)
+        self.cursor.execute(str(sql))
+        res = self.cursor.fetchone()
+        if res is None:
+            return None
         return res[0]
--- db/exceptions.py
+++
@@ -1,2 +0,0 @@
-class FNSUserDoesNotExistException(Exception):
-    pass
