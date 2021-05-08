353b5beb8a54c654144c15bce40cf98bb2859caa
Ilya Vologin <ilyavologin@gmail.com>
2021-05-08 12:01:27 +0300

    Remove pyzbar and reformat files

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import bot
 import os
 
+
 def main():
-    TOKEN = os.environ.get("TOKEN")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    fns_bot = bot.Bot(TOKEN, DATABASE_URL)
+    token = os.environ.get("TOKEN")
+    database_url = os.environ.get("DATABASE_URL")
+    fns_bot = bot.Bot(token, database_url)
     fns_bot.run()
 
 
 if __name__ == "__main__":
     main()
-
